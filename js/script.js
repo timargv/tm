38a99c199e0744be1964976527affd213920f2f5
timargv <tima.rgv@mail.ru>
2018-04-23 15:57:48 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 6,
+  spaceBetween: 10,
+  slidesPerGroup: 6,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.carousel-control-next',
+    prevEl: '.carousel-control-prev',
+  },
 
+});
+
+
+$('#myDropdown .dropdown-menu').on({
+	"click":function(e){
+      e.stopPropagation();
+    }
+});
