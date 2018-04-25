be3be90103fc824c038fc808c9b4e2488af4ec25
timargv <tima.rgv@mail.ru>
2018-04-25 15:30:41 +0300

    update

--- js/script.js
+++ js/script.js
@@ -37,22 +37,15 @@ $('#myDropdown .dropdown-menu').on({
 	});
 })(jQuery);
 
-
-//
-// $(document).ready(function() {
-//
-//     $( ".dropdown-submenu" ).click(function(event) {
-//         // stop bootstrap.js to hide the parents
-//         event.stopPropagation();
-//         // hide the open children
-//         $( this ).find(".dropdown-submenu").removeClass('open');
-//         // add 'open' class to all parents with class 'dropdown-submenu'
-//         $( this ).parents(".dropdown-submenu").addClass('open');
-//         // this is also open (or was)
-//         $( this ).toggleClass('open');
+// document.addEventListener('DOMContentLoaded', function () {
+//     $('.cat-btn-header').on('click', function () {
+//       $(this)
+//         .find('[data-fa-i2svg]')
+//         .toggleClass('fa-bars')
+//         .toggleClass('fa-angle-down');
 //     });
-//
-// });
+//   });
+
 $('.cat-btn-header').click(function(e){
 		 e.preventDefault();
      $('.h-catalog-overlay').addClass('fixed');
